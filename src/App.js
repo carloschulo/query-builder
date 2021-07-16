7ad3f31b68f3bb6e2348eb6d7f417c1c84dd1def
Carlos Henriquez <carloschulo@gmail.com>
2021-07-15 21:31:06 -0400

    Adding rough prototype with string types

--- src/App.js
+++ src/App.js
@@ -1,10 +1,51 @@
 import "./styles.css";
+import buildQuery, {
+  getNumberOperators,
+  getStringOperators,
+  predicatesList,
+  predicates,
+} from "./useQueryBuilder";
+
+import { useState } from "react";
 
 export default function App() {
+  const [select, setSelect] = useState("Email");
+  const [select2, setSelect2] = useState(select);
+
+  const [value1, setValue1] = useState("");
+  const handleSelectChange = (e) => {
+    setSelect(e.target.value);
+  };
+  const handleSelect2Change = (e) => {
+    setSelect2(e.target.value);
+  };
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <div>{console.log(predicates[select])}</div>
+      <select value={select} onChange={handleSelectChange}>
+        {predicatesList.map((op, index) => (
+          <option value={op} key={op}>
+            {op}
+          </option>
+        ))}
+      </select>
+      {select ? (
+        <select value={select2} onChange={handleSelect2Change}>
+          {predicates[select].operators().map((op, index) => (
+            <option value={op} key={op}>
+              {op}
+            </option>
+          ))}
+        </select>
+      ) : null}
+      {predicates[select].type === "string" ? (
+        <input
+          type="text"
+          value={value1}
+          onChange={(e) => setValue1(e.target.value)}
+        />
+      ) : null}
+      <div>{buildQuery(select, select2, value1)}</div>
     </div>
   );
 }
--- src/useQueryBuilder.js
+++ src/useQueryBuilder.js
@@ -1,14 +1,8 @@
 const TABLE = "session";
 
-function getStringOperators(
-  operator,
-  columnName,
-  value1,
-  value2,
-  ...valueList
-) {
+function getStringOperators(operator, columnName, value1, ...valueList) {
   const queryObject = {
-    equals: `WHERE ${value1} = ${value2}`,
+    equals: `WHERE ${columnName} = ${value1}`,
     contains: `WHERE ${columnName} LIKE '%${value1}%'`,
     "starts with": `WHERE  LIKE '${value1}%'`,
     "in list": `WHERE ${columnName} IN (${valueList})`,
@@ -41,24 +35,60 @@ function getNumberOperators(
 }
 
 const predicates = {
-  Email: { column: "user_email", operators: getStringOperators },
-  "Screen Width": { column: "screen_width", operators: getNumberOperators },
-  "Screen Height": { column: "screen_height", operators: getNumberOperators },
-  "# of Visits": { column: "visits", operators: getNumberOperators },
-  "First Name": { column: "user_first_name", operators: getStringOperators },
-  "Last Name": { column: "user_last_name", operators: getStringOperators },
+  Email: {
+    column: "user_email",
+    operators: getStringOperators,
+    type: "string",
+  },
+  "Screen Width": {
+    column: "screen_width",
+    operators: getNumberOperators,
+    type: "number",
+  },
+  "Screen Height": {
+    column: "screen_height",
+    operators: getNumberOperators,
+    type: "number",
+  },
+  "# of Visits": {
+    column: "visits",
+    operators: getNumberOperators,
+    type: "number",
+  },
+  "First Name": {
+    column: "user_first_name",
+    operators: getStringOperators,
+    type: "string",
+  },
+  "Last Name": {
+    column: "user_last_name",
+    operators: getStringOperators,
+    type: "string",
+  },
   "Page Response time (ms)": {
     column: "page_response",
     operators: getNumberOperators,
+    type: "number",
   },
-  Domain: { column: "domain", operators: getStringOperators },
-  "Page Path": { column: "path", operators: getStringOperators },
-  Id: { column: "id", operators: getNumberOperators },
+  Domain: { column: "domain", operators: getStringOperators, type: "string" },
+  "Page Path": {
+    column: "path",
+    operators: getStringOperators,
+    type: "string",
+  },
+  Id: { column: "id", operators: getNumberOperators, type: "number" },
 };
 
-const criteriaList = [];
+const predicatesList = Object.keys(predicates);
 
-const setCriteriaList = (predicate, operator, value1, value2, valueList) => {
+export default function buildQuery(
+  predicate,
+  operator,
+  value1,
+  value2,
+  valueList
+) {
+  const criteriaList = [];
   criteriaList.push(
     predicates[predicate].operators(
       operator,
@@ -68,15 +98,28 @@ const setCriteriaList = (predicate, operator, value1, value2, valueList) => {
       valueList
     )
   );
-};
 
-setCriteriaList("Email", "equals", 1, 1);
-setCriteriaList("Screen Width", "greater than", 10);
-setCriteriaList("Screen Width", "in list", null, null, [1, 2, 3, 4]);
-// console.log(getNumberOperators());
-// console.log(getStringOperators());
+  // const setCriteriaList = (predicate, operator, value1, value2, valueList) => {
+  //   criteriaList.push(
+  //     predicates[predicate].operators(
+  //       operator,
+  //       predicates[predicate].column,
+  //       value1,
+  //       value2,
+  //       valueList
+  //     )
+  //   );
+  // };
 
-const select = `SELECT * from ${TABLE}`;
-criteriaList;
-const finalQuery = select.concat(" " ,criteriaList.join(" AND "));
-finalQuery;
+  // setCriteriaList("Email", "equals", 1, 1);
+  // setCriteriaList("Screen Width", "greater than", 10);
+  // setCriteriaList("Screen Width", "in list", null, null, [1, 2, 3, 4]);
+  // console.log(getNumberOperators());
+  // console.log(getStringOperators());
+
+  const select = `SELECT * FROM ${TABLE}`;
+  const finalQuery = select.concat(" ", criteriaList.join(" AND ")) || "";
+  return finalQuery;
+}
+
+export { getNumberOperators, getStringOperators, predicatesList, predicates };
